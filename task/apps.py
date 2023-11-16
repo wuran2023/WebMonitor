9a9f79f494043ebd7f2df98a6f9b16322f577aed
wuran2023 <138594337+wuran2023@users.noreply.github.com>
2023-11-16 21:26:31 +0800

    Update apps.py

--- task/apps.py
+++ task/apps.py
@@ -4,3 +4,23 @@ from django.apps import AppConfig
 class TaskConfig(AppConfig):
     name = 'task'
     verbose_name = '任务管理'
+
+    def ready(self):
+        try:
+            from .models import TaskStatus, Task, RSSTask  # 导入任务模型
+            from task.utils.scheduler import add_job  # 导入用于启动任务的函数
+
+            tasks_to_start = TaskStatus.objects.filter(task_status=0)  # 获取所有应该运行的任务
+            for task_status in tasks_to_start:
+                # 获取与 TaskStatus 相关联的 Task 或 RSSTask 实例
+                if task_status.task_type == 'html':
+                    task = Task.objects.get(id=task_status.task_id)
+                    add_job(task.id, task.frequency, type='html')
+                elif task_status.task_type == 'rss':
+                    task = RSSTask.objects.get(id=task_status.task_id)
+                    add_job(task.id, task.frequency, type='rss')
+
+        except OperationalError:
+            print('数据库尚未准备好')
+            # 处理数据库尚未准备好的情况
+            pass
