fd4b77f73d903994d53e854a55fa69d3ee057064
wuran2023 <138594337+wuran2023@users.noreply.github.com>
2023-11-17 18:08:02 +0800

    Update apps.py

--- task/apps.py
+++ task/apps.py
@@ -1,26 +1,35 @@
 from django.apps import AppConfig
-
+from django.db.utils import OperationalError
+from apscheduler.schedulers.background import BackgroundScheduler
 
 class TaskConfig(AppConfig):
     name = 'task'
     verbose_name = '任务管理'
 
-    def ready(self):
-        try:
-            from .models import TaskStatus, Task, RSSTask  # 导入任务模型
-            from task.utils.scheduler import add_job  # 导入用于启动任务的函数
+    scheduler = BackgroundScheduler()
 
-            tasks_to_start = TaskStatus.objects.filter(task_status=0)  # 获取所有应该运行的任务
+    def ready(self):
+        self.scheduler.add_job(
+            self.start_tasks,
+            'interval',
+            minutes=240,  # 每隔4小时运行一次
+        )
+        self.scheduler.start()
+
+    def start_tasks(self):
+        try:
+            from .models import TaskStatus, Task, RSSTask
+            from task.utils.scheduler import add_job, monitor
+
+            tasks_to_start = TaskStatus.objects.filter(task_status=0)
             for task_status in tasks_to_start:
-                # 获取与 TaskStatus 相关联的 Task 或 RSSTask 实例
                 if task_status.task_type == 'html':
                     task = Task.objects.get(id=task_status.task_id)
                     add_job(task.id, task.frequency, type='html')
                 elif task_status.task_type == 'rss':
                     task = RSSTask.objects.get(id=task_status.task_id)
                     add_job(task.id, task.frequency, type='rss')
-
+                  
         except OperationalError:
             print('数据库尚未准备好')
-            # 处理数据库尚未准备好的情况
             pass
